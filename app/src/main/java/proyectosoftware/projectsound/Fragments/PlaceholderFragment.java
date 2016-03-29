6392650955efcb5be0e8035ea461653adcd39dae
novales35 <guillermocebollero@gmail.com>
2016-03-29 21:29:42 +0200

    Simplificación del fragmento base

--- app/src/main/java/proyectosoftware/projectsound/Fragments/PlaceholderFragment.java
+++ app/src/main/java/proyectosoftware/projectsound/Fragments/PlaceholderFragment.java
@@ -17,39 +17,20 @@ public class PlaceholderFragment extends Fragment {
      * Este argumento del fragmento representa el título de cada
      * sección
      */
-    public static final String ARG_SECTION_TITLE = "section_number";
     public static final String ARG_LAYOUT = "Layout";
 
 
     public PlaceholderFragment() {
     }
 
-    /**
-     * Crea una instancia prefabricada de {@link PlaceholderFragment}
-     *
-     * @param sectionTitle Título usado en el contenido
-     * @return Instancia dle fragmento
-     */
-    public static PlaceholderFragment newInstance(String sectionTitle, int layout) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_SECTION_TITLE, sectionTitle);
-        args.putInt(ARG_LAYOUT, layout);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        int layout = getArguments().getInt(ARG_LAYOUT);
-        View view = inflater.inflate(layout, container, false);
+        //TODO set layout
+        //View view = inflater.inflate(layout, container, false);
 
-        // Ubicar argumento en el text view de section_fragment.xml
-        String title = getArguments().getString(ARG_SECTION_TITLE);
-        TextView titulo = (TextView) view.findViewById(R.id.title);
-        titulo.setText(title);
-        return view;
+        //return view;
+        return null;
     }
 
 }
