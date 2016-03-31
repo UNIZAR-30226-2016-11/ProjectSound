6ffbb4fd78e881b28dca56efe1dba47b3ee27393
novales35 <guillermocebollero@gmail.com>
2016-03-31 14:08:37 +0200

    Añadido opción de rellenado por argumentos

--- app/src/main/java/proyectosoftware/projectsound/Fragments/SongsFragment.java
+++ app/src/main/java/proyectosoftware/projectsound/Fragments/SongsFragment.java
@@ -1,5 +1,6 @@
 package proyectosoftware.projectsound.Fragments;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -7,9 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import proyectosoftware.projectsound.CustomAdapters.DbAdapter;
 import proyectosoftware.projectsound.R;
 import proyectosoftware.projectsound.Tipos.Song;
 import proyectosoftware.projectsound.CustomAdapters.SongAdapter;
@@ -23,8 +26,8 @@ public class SongsFragment extends Fragment {
      * sección
      */
     public static final String ARG_SECTION_TITLE = "section_number";
-    public static final String ARG_LAYOUT = "Layout";
-
+    public static final String ARG_PLAYLIST = "play";
+    private DbAdapter mDb;
 
     public SongsFragment() {
     }
@@ -33,17 +36,32 @@ public class SongsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.song_layout, container, false);
+        ArrayList<Song> canciones = new ArrayList<Song>();
+        //Ponemos el titulo a la pestaña
         getActivity().setTitle("Canciones");
+        //Accedemos a la base de datos
+        mDb = new DbAdapter(view.getContext());
+        mDb.open();
+        //obtenemos las canciones
+        Cursor mCursor = mDb.getAllFromPlaylist(getArguments().get(ARG_PLAYLIST).toString());
+        if (mCursor.moveToFirst()) {
+            do {
+                String titulo = mCursor.getString(mCursor.getColumnIndex(DbAdapter.KEY_TITULO));
+                String path = mCursor.getString(mCursor.getColumnIndex(DbAdapter.KEY_RUTA));
+                boolean fav = (mCursor.getInt(mCursor.getColumnIndex(DbAdapter.KEY_FAVORITO))==1);
+                int duration = mCursor.getInt(mCursor.getColumnIndex(DbAdapter.KEY_DURACION));
+                int reproductions = mCursor.getInt(mCursor.getColumnIndex(DbAdapter.KEY_REPRODUCCIONES));
+                canciones.add(new Song(titulo,path,fav,duration,reproductions));
+            } while (mCursor.moveToNext());
+        }
+        mCursor.close();
+
         TabLayout tabs = (TabLayout) view.findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("TODAS"));
         tabs.addTab(tabs.newTab().setText("DURACIÓN"));
         tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
 
-        ArrayList<Song> canciones = new ArrayList<Song>();
-        canciones.add(new Song("Canción 1","",false,1,1));
-        canciones.add(new Song("Canción 2","",true,2,2));
-        //canciones.add("C1");
-        //canciones.add("C2");
+
         ListView listView_songs = (ListView) view.findViewById(R.id.listview_canciones);
         SongAdapter adaptador = new SongAdapter(view.getContext(), canciones);
         listView_songs.setAdapter(adaptador);
@@ -51,7 +69,7 @@ public class SongsFragment extends Fragment {
                 new TabLayout.OnTabSelectedListener() {
                     @Override
                     public void onTabSelected(TabLayout.Tab tab) {
-                        // ...
+                        Toast.makeText(getContext(),"Ordenación no disponible",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
