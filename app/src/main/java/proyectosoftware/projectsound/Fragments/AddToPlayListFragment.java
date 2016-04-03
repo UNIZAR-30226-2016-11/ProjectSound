079fd39e0099a5f4952eea2c92f9c73b094502df
danirueda <danir94@gmail.com>
2016-04-03 13:07:50 +0200

    Todas las canciones sacadas, ahora faltan las de un playlist

--- app/src/main/java/proyectosoftware/projectsound/Fragments/AddToPlayListFragment.java
+++ app/src/main/java/proyectosoftware/projectsound/Fragments/AddToPlayListFragment.java
@@ -1,7 +1,10 @@
 package proyectosoftware.projectsound.Fragments;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import proyectosoftware.projectsound.CustomAdapters.DbAdapter;
 import proyectosoftware.projectsound.R;
 
 /**
  * Fragmento para el contenido principal
  */
 public class AddToPlayListFragment extends Fragment {
+
+    public static final String ARG_PLAYLIST = "play";
 
     public AddToPlayListFragment() {
     }
@@ -26,6 +32,29 @@ public class AddToPlayListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_to_playlist, container, false);
+        Context contexto = getContext();
+        DbAdapter datos = new DbAdapter(contexto);
+        Cursor c = datos.getAllCancion();
+        //Obtener todas las canciones e introducirlas en un ArrayList
+        List<String> todasCanciones = new ArrayList<String>();
+        //ruta, titulo, reporducciones, duracion, favorita
+        if(c.moveToFirst()){
+            for (int i=0; i<c.getCount();i++){ //Canciones de toda la BBDD
+                Log.d("Olia",c.getString(c.getColumnIndex(DbAdapter.KEY_TITULO)));
+                //todasCanciones.add(c.getString(1));
+                c.moveToNext();
+            }
+        }
+        //SACAR CANCIONES DE UN PLAYLIST, todo rulando hasta este punto
+        c = datos.getAllFromPlaylist(getArguments().getString("Calle 13"));//En realidad va ARG_PLAYLIST
+        List<String> cancionesPlaylist = new ArrayList<String>();
+        String [] caca = c.getColumnNames();
+        for(int i=0;i<caca.length;i++){
+            Log.d("Playlist",caca[i]);
+        }
+
+        TextView nombreLista = (TextView) view.findViewById(R.id.nombreLista);
+        nombreLista.setText("Favoritos");
 
         /*//Canciones
         List<String> canciones = new ArrayList<String>();
@@ -50,5 +79,4 @@ public class AddToPlayListFragment extends Fragment {
         return view;
 
     }
-
 }
