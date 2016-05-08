ae7cff418df537a46f4fc499d50819183c02121a
danirueda <danir94@gmail.com>
2016-05-08 20:19:45 +0200

    Quitadas dos líneas de código que servían para depurar

--- app/src/main/java/proyectosoftware/projectsound/Fragments/AddToPlayListFragment.java
+++ app/src/main/java/proyectosoftware/projectsound/Fragments/AddToPlayListFragment.java
@@ -127,11 +127,9 @@ public class AddToPlayListFragment extends Fragment {
         try{
             /* Solo falta utilizar los metodos de la BBDD cuando este terminada*/
             for(int i=0;i<porAniadir.size();i++){
-                Log.d("A veeerrr",porAniadir.get(i).getTitle());
                 datos.insertToPlaylist(porAniadir.get(i).getPath(),playlist);
             }
             for(int i=0;i<porBorrar.size();i++){
-                Log.d("Conteoooo", porBorrar.get(i).getTitle());
                 datos.deleteFromPlaylist(porBorrar.get(i).getPath(),playlist);
             }
             Toast.makeText(getContext(),"Cambios guardados",Toast.LENGTH_LONG).show();
